d9b4f47bff32d058831058321c536bef67aa8c98
emily <emily@Emilys-MacBook-Pro.local>
2022-06-19 09:29:02 -0600

    created the html components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +9,4 @@ import { Observable } from 'rxjs';
 
 export class AppComponent {
   title = 'path-assessment-angular';
-
-  sheetUrl= 'https://script.google.com/macros/s/AKfycbwG6wmBMTXx6_w3t4UMIP5G0ZuZv20cJSYY-d14XPboXUFYLVNFkOqYG623iKXGvVepVA/exec';
 }
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  public questionList: any=[];
+  public currentQuestion:number=0;
+  
+
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
+
+  //     this.router.navigateByUrl('/result')
+    
 }
